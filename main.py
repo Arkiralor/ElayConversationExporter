7fca4c96f9ed6953cfc1dd3f0bd68cb020882dc4
prithoomedhi <prithoo.medhi@mslate.ai>
2025-04-10 11:01:41 +0530

    added arg support for runtime

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import re
 CHATBOT_ID = 2848
 DATE_RANGE_START = "2025-03-06 00:00:00"
 DATE_RANGE_END = "2025-04-07 23:59:59"
+
 FILE_PATH = f"data{sep}{CHATBOT_ID}-convo.csv"
 URL_REGEX = re.compile(
     r'^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,}){1,2}\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,}){1,2})|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?')
+DEBUG:bool = False
 load_dotenv()
 
 
@@ -41,6 +43,7 @@ def get_raw_export():
     """
 
     with engine.connect() as conn:
+        print(f"Executing query with parameters: {CHATBOT_ID=}, {DATE_RANGE_START=}, {DATE_RANGE_END=}")
         result = conn.execute(
             text(prepared_statement),
             {
@@ -50,13 +53,12 @@ def get_raw_export():
             }
         )
 
+        print(f"Query executed successfully. {result.rowcount} rows returned.")
         rows = result.fetchall()
         data = []
 
         for row in rows:
-            # print(row)
-            data.append(
-                {
+            data_dict = {
                     "id": row.id,
                     "assistant_id": row.assistant_id,
                     "raw_data": dumps(row.raw_data),
@@ -64,7 +66,9 @@ def get_raw_export():
                     "extra_data": dumps(row.extra_data),
                     "created_at": row.created_at
                 }
-            )
+            if DEBUG:
+                print(f"{data_dict=}")
+            data.append(data_dict)
         df = pd.DataFrame(data)
         df.to_csv(FILE_PATH, index=False)
         print(f"Exported {len(data)} records to {FILE_PATH}")
@@ -94,18 +98,27 @@ def export_conversation():
             answer = data_001.get(key)
             if answer and isinstance(answer, str) and re.match(URL_REGEX, answer):
                 continue
+            if DEBUG:
+                print(f"Key: {key}, Answer: {answer}")
             full_data[key] = answer
 
         for key in data_002:
             answer = data_002.get(key)
             if answer and isinstance(answer, str) and re.match(URL_REGEX, answer):
                 continue
+            if DEBUG:
+                print(f"Key: {key}, Answer: {answer}")
             full_data[key] = answer
 
         convo_json.append(full_data)
 
     df = pd.DataFrame(convo_json)
-    df.to_csv(f"data{sep}export{sep}{CHATBOT_ID}_export_{DATE_RANGE_START}_to_{DATE_RANGE_END}.csv")
+
+    start = DATE_RANGE_START.split(" ")[0].replace("-", "_").replace(":", "_")
+    end = DATE_RANGE_END.split(" ")[0].replace("-", "_").replace(":", "_")
+    output_file = f"data{sep}export{sep}{CHATBOT_ID}_export_{start}_to_{end}.csv"
+    df.to_csv(output_file, encoding="utf-8")
+    print(f"Exported conversation data to {output_file}")
 
 
 def run():
@@ -116,29 +129,35 @@ def run():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Export chatbot conversation data.")
-    # parser.add_argument(
-    #     "--get_raw_export",
-    #     action="store_true",
-    #     help="Export raw conversation data from the database."
-    # )
-    # parser.add_argument(
-    #     "--export_cleaned_conversation",
-    #     action="store_true",
-    #     help="Export cleaned conversation data."
-    # )
-    # parser.add_argument(
-    #     "--export_conversation",
-    #     action="store_true",
-    #     help="Export conversation data to CSV."
-    # )
-    # args = parser.parse_args()
-    # if args.get_raw_export:
-    #     get_raw_export()
-    # if args.export_cleaned_conversation:
-    #     export_cleaned_conversation()
-    # if args.export_conversation:
-    #     export_conversation()
-    # if not (args.get_raw_export or args.export_cleaned_conversation or args.export_conversation):
-    #     print("No arguments provided. Running all tasks.")
-    #     # Run all tasks if no specific argument is provided
+    parser.add_argument(
+        "--chatbot_id",
+        type=int,
+        help="Chatbot ID to filter the conversation data."
+    )
+    parser.add_argument(
+        "--start_date",
+        type=str,
+        help="Start date for the conversation data range."
+    )
+    parser.add_argument(
+        "--end_date",
+        type=str,
+        help="End date for the conversation data range."
+    )
+    parser.add_argument(
+        "--debug",
+        type=str,
+        default=False,
+        help="Enable debug mode."
+    )
+    args = parser.parse_args()
+    if args.chatbot_id is not None and isinstance(args.chatbot_id, int):
+        CHATBOT_ID = args.chatbot_id
+    if args.start_date is not None and isinstance(args.start_date, str):
+        DATE_RANGE_START = args.start_date
+    if args.end_date is not None and isinstance(args.end_date, str):
+        DATE_RANGE_END = args.end_date
+    if args.debug and isinstance(args.debug, str):
+        DEBUG = eval(args.debug.lstrip().rstrip().title())
+        print(f"Debug mode is set to {DEBUG}.")
     run()
